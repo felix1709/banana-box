4101b776b805cd6018ac1b711a75a7045c622684
banana-box <dev@bananabox.local>
2026-06-27 16:54:40 +0800

    feat(rust): tray + global shortcut + blur-hide

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,65 @@
 mod commands;
 mod library;
 
+use tauri::{Manager, WindowEvent};
+use tauri::tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent};
+use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(
+            tauri_plugin_global_shortcut::Builder::new()
+                .with_handler(|app, _shortcut, event| {
+                    if event.state == ShortcutState::Pressed {
+                        toggle_panel(app);
+                    }
+                })
+                .build(),
+        )
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_opener::init())
+        .setup(|app: &mut tauri::App| -> Result<(), Box<dyn std::error::Error>> {
+            // 注册全局快捷键 Ctrl+Shift+B
+            let shortcut = Shortcut::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyB);
+            app.global_shortcut().register(shortcut)?;
+
+            // 系统托盘
+            TrayIconBuilder::with_id("main")
+                .tooltip("Banana Box")
+                .icon(app.default_window_icon().expect("icon").clone())
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } = event
+                    {
+                        toggle_panel(tray.app_handle());
+                    }
+                })
+                .menu(
+                    &tauri::menu::MenuBuilder::new(app)
+                        .item(&tauri::menu::MenuItemBuilder::with_id("show", "显示").build(app)?)
+                        .item(&tauri::menu::MenuItemBuilder::with_id("quit", "退出").build(app)?)
+                        .build()?,
+                )
+                .on_menu_event(|app, event| match event.id().as_ref() {
+                    "show" => toggle_panel(app),
+                    "quit" => app.exit(0),
+                    _ => {}
+                })
+                .build(app)?;
+            Ok(())
+        })
+        .on_window_event(|window, event| {
+            // 失焦自动隐藏
+            if let WindowEvent::Focused(false) = event {
+                let _ = window.hide();
+            }
+        })
         .invoke_handler(tauri::generate_handler![
             commands::load_library,
             commands::save_library,
@@ -21,4 +72,15 @@ pub fn run() {
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn toggle_panel(app: &tauri::AppHandle) {
+    if let Some(win) = app.get_webview_window("main") {
+        if win.is_visible().unwrap_or(false) {
+            let _ = win.hide();
+        } else {
+            let _ = win.show();
+            let _ = win.set_focus();
+        }
+    }
 }
